f1ea24cbed996b14e25b63d416db9ebee7cd5015
clydehsieh <clydehsieh0601@gmail.com>
2015-12-22 10:13:13 +0800

    Initial Commit

---
+++ PlugTrip_ios/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PlugTrip_ios
+//
+//  Created by Chin-Hui Hsieh  on 12/22/15.
+//  Copyright © 2015 Chin-Hui Hsieh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PlugTrip_ios/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PlugTrip_ios
+//
+//  Created by Chin-Hui Hsieh  on 12/22/15.
+//  Copyright © 2015 Chin-Hui Hsieh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
